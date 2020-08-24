2fbd510333bf152d3b191ca1c977880b50edc4f4
Marlyn Ariana Juárez Cordova <201800109@colsanjose.edu.gt>
2020-08-24 10:31:57 -0600

    Repositorio.

---
+++ Controller/Home.Controller.php
@@ -0,0 +1,23 @@
+<?php
+	
+	class Home
+	{
+		public function Inicio()
+		{
+			$smarty=new Smarty();
+
+			$smarty->display('master.tpl');
+		}
+
+		/*public function User()
+		{
+			$user=new Usuarios();
+			$user->BuscarUsuario();
+
+			//$class2=new Librerias();
+			//$class2->Libreria1();
+
+			//$smarty=new Smarty();
+		}*/
+	}
+?>

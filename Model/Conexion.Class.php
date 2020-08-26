3f24a460b66b0de8379578e05e39f11a904f5871
victorandrez <62565150+victorandrez@users.noreply.github.com>
2020-08-26 11:52:04 -0600

    Update Conexion.Class.php

--- Model/Conexion.Class.php
+++ Model/Conexion.Class.php
@@ -14,7 +14,7 @@
             $this->query("SET NAMES 'utf8';");
             $this->connect_errno ? die('Error en la conexion de la base de datos.') : $error="<br><br> Conectado con la base de datos: ".$base;
             //echo $error;
-			unset($error);
+		   unset($error);
        }
      }
 ?>

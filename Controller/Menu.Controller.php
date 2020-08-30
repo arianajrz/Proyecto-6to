cf898bcfe5c0c456b1a3ae7bd028a86be68fd551
olga0236 <62441263+olga0236@users.noreply.github.com>
2020-08-29 18:06:23 -0600

    Update Menu.Controller.php

--- Controller/Menu.Controller.php
+++ Controller/Menu.Controller.php
@@ -6,18 +6,34 @@
 			$smarty=new Smarty();
 			session_start();
 			$_SESSION['opcion']=$_GET['opcion'];
-			 if($_SESSION['opcion']=='VerUsuarios')
-            {
-                 $inv=new Can();
-                 $lib=new Librerias(); 
-                 $invent=$inv->VerUsuarios();
-                 $dato=$lib->DatosSmarty($invent);
-                 $smarty->assign('dato',$dato);
-            }
+			if($_SESSION['opcion']=='VerUsuarios')
+			{
+				$inv=new Usuarios();
+				$lib=new Librerias(); 
+				$invent=$inv->VerUsuarios();
+				$dato=$lib->DatosSmarty($invent);
+				$smarty->assign('dato',$dato);
+			}
+			else if($_SESSION['opcion']=='VerLibros')
+			{
+				$inv=new Inventario();
+				$lib=new Librerias(); 
+				$invent=$inv->VerLibros();
+				$dato=$lib->DatosSmarty($invent);
+				$smarty->assign('dato',$dato);
+			}
+			else if($_SESSION['opcion']=='VerUniformes')
+			{
+				$inv=new Inventario();
+				$lib=new Librerias(); 
+				$invent=$inv->VerUnifomes();
+				$dato=$lib->DatosSmarty($invent);
+				$smarty->assign('dato',$dato);
+			}
 			$smarty->assign('e','null');
-            $smarty->assign('vista',$_SESSION['opcion']);
-            $smarty->assign('usuario',$_SESSION['user']);
-            $smarty->display('Master.tpl');
+            		$smarty->assign('vista',$_SESSION['opcion']);
+            		$smarty->assign('usuario',$_SESSION['user']);
+            		$smarty->display('Master.tpl');
 		}
 	}
 

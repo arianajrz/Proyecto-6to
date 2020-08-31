217dfb4a99026c10fce13c5064599bc1c9366b67
olga0236 <62441263+olga0236@users.noreply.github.com>
2020-08-30 18:10:46 -0600

    Update Invent.Controller.php

--- Controller/Invent.Controller.php
+++ Controller/Invent.Controller.php
@@ -10,10 +10,10 @@
 			$nombre=$_POST['nombre'];
 			$materia=$_POST['materia'];
 			$grado=$_POST['grado'];
-      $autor=$_POST['autor'];
+      			$autor=$_POST['autor'];
 			$editorial=$_POST['editorial'];
 			$precio=$_POST['precio'];
-      $cant=$_POST['cant'];
+      			$cant=$_POST['cant'];
 
 			$resultado=$gl->AgregarLibro($nombre,$materia,$grado,$autor,$editorial,$precio,$cant);
 			if ($resultado)
@@ -31,8 +31,8 @@
 			$smarty->display('Master.tpl'); 
 		}
 		
-	    public function QuitarLibros()
-	    {
+	   	public function QuitarLibros()
+	    	{
             		$ql=new Inventario();
 			$smarty=new Smarty();
 			session_start();
@@ -40,10 +40,10 @@
 			$nombre=$_POST['nombre'];
 			$materia=$_POST['materia'];
 			$grado=$_POST['grado'];
-      $autor=$_POST['autor'];
+      			$autor=$_POST['autor'];
 			$editorial=$_POST['editorial'];
 			$precio=$_POST['precio'];
-      $cant=$_POST['cant'];
+      			$cant=$_POST['cant'];
 
 			$resultado=$ql->RetirarLibro($nombre,$materia,$grado,$autor,$editorial,$precio,$cant);
 			if ($resultado)
